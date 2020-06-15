6f7cb283228fb63edf8516d99193de7454d4d947
Yana <kittijane@mail.ru>
2020-06-15 23:47:22 +0300

    it is scary but it moves

--- lab6server/src/com/company/ServerStart.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class ServerStart {
-    public static void main(String[] args) {
-        ServerPart server = new ServerPart(3562, System.getenv("envvar"));
-        server.init();
-    }
-}
